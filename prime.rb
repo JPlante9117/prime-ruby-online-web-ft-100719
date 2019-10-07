e7432ec7ff8a3adb31d288d110e28901e7a98df5
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 19:10:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 # Add  code here!
 
 def prime?(num)
+  is_prime? = false
   if num == 2 || num == 3 || num == 5
     is_prime? = true
   elsif num != 0 && num != 1 && num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num > 0
