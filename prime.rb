7cb8cd725d410894917c737d5e1840df48055f43
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 19:03:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(num)
   if num == 2 || num == 3 || num == 5
     return true
-  elsif num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num > 0
+  elsif num != 0 && num != 1 && num % 2 != 0 && num % 3 != 0 && num % 5 != 0 && num > 0
     return true
   else
     return false
