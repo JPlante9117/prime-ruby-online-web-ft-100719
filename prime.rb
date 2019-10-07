03d49883a1a2e7cdcd17115f3af6a08c319ba3fe
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 19:32:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,9 +5,11 @@ def prime?(num)
   divisor = (2..1000).to_a
   if num == 2 || num == 3 || num == 5
     is_prime = true
+  elsif num < 0
+    return false
   else
     divisor.each do |number|
-     if num % number == 0 && num != number && num > 0
+     if num % number == 0 && num != number
       is_prime = false
      end
     end
