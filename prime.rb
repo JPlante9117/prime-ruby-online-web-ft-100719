43da5a97f2266ab924d68f69d31f19ff01c619af
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 19:01:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,9 @@
 # Add  code here!
 
 def prime?(num)
-  if num % 2 != 0 && num % 3 != 0 && num % 5 != 0
+  if num == 2 || num == 3 || num == 5
+    return true
+  elsif num % 2 != 0 && num % 3 != 0 && num % 5 != 0
     return true
   else
     return false
